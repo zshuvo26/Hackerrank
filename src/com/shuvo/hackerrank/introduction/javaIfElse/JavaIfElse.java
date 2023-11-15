0d8dafc1311824b4d0d6f4be0f7cfeedd5e8c81b
asm.zakaria <zshuvo26@gmail.com>
2023-11-15 13:56:18 +0600

    Java If-Else

---
+++ src/com/shuvo/hackerrank/introduction/javaIfElse/JavaIfElse.java
@@ -0,0 +1,14 @@
+package com.shuvo.hackerrank.introduction.javaIfElse;
+
+import java.util.Scanner;
+
+public class JavaIfElse {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
